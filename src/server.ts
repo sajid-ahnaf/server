8d67c3361a85927d5b1cf79c8a91f0db50c45c27
MD. SAJID HASSAN <sajidhasan2023@gmail.com>
2024-02-22 13:35:22 +0600

    test.ogado

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 // conncet with mongodb atlas
-const mongoUrl = `mongodb+srv://sajidhasanmist:Allah4937@cluster0.srphyvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const mongoUrl = `mongodb+srv://sajidhasanmist:Allah4937@cluster0.srphyvi.mongodb.net/test-orgadosajid?retryWrites=true&w=majority&appName=Cluster0`;
 const mongooseOptions: any = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
